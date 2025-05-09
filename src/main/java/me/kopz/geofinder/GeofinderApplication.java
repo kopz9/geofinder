73bd1bc7446f11536653dbd9f8f80df6cd928b96
kopz9 <gustavoeduardo438@gmail.com>
2025-05-08 21:34:26 -0300

    first commit

---
+++ src/main/java/me/kopz/geofinder/GeofinderApplication.java
@@ -0,0 +1,13 @@
+package me.kopz.geofinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeofinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeofinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/kopz/geofinder/GeofinderApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kopz.geofinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeofinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
